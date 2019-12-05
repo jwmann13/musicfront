fbada4950a5960db3302edaba93cb6811b3a33c0
Jeffrey Mann <jwmann22@gmail.com>
2019-12-05 18:00:25 -0500

    ran linter for the first time

--- controllers/homepage_controller.js
+++ controllers/homepage_controller.js
@@ -1,61 +1,61 @@
-const express = require('express');
+const express = require("express");
 
 let router = express.Router();
-let db = require('../models');
+let db = require("../models");
 
 // HTML ROUTES
 // Load index page
-router.get("/", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-        res.render("index", {
-            msg: "Welcome!",
-            examples: dbExamples
-        });
+router.get("/", function(req, res) {
+  db.Example.findAll({}).then(function(dbExamples) {
+    res.render("index", {
+      msg: "Welcome!",
+      examples: dbExamples
     });
+  });
 });
 
 // Load example page and pass in an example by id
-router.get("/example/:id", function (req, res) {
-    db.Example.findOne({
-        where: {
-            id: req.params.id
-        }
-    }).then(function (dbExample) {
-        res.render("example", {
-            example: dbExample
-        });
+router.get("/example/:id", function(req, res) {
+  db.Example.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(dbExample) {
+    res.render("example", {
+      example: dbExample
     });
+  });
 });
 
 // Render 404 page for any unmatched routes
-router.get("*", function (req, res) {
-    res.render("404");
+router.get("*", function(req, res) {
+  res.render("404");
 });
 
 // API ROUTES
 // Get all examples
-router.get("/api/examples", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-        res.json(dbExamples);
-    });
+router.get("/api/examples", function(req, res) {
+  db.Example.findAll({}).then(function(dbExamples) {
+    res.json(dbExamples);
+  });
 });
 
 // Create a new example
-router.post("/api/examples", function (req, res) {
-    db.Example.create(req.body).then(function (dbExample) {
-        res.json(dbExample);
-    });
+router.post("/api/examples", function(req, res) {
+  db.Example.create(req.body).then(function(dbExample) {
+    res.json(dbExample);
+  });
 });
 
 // Delete an example by id
-router.delete("/api/examples/:id", function (req, res) {
-    db.Example.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then(function (dbExample) {
-        res.json(dbExample);
-    });
+router.delete("/api/examples/:id", function(req, res) {
+  db.Example.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(dbExample) {
+    res.json(dbExample);
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,31 +1,38 @@
-const express = require('express');
-const exphbrs = require('express-handlebars');
-const path = require('path');
+const express = require("express");
+const exphbrs = require("express-handlebars");
+const path = require("path");
 
 const PORT = process.env.PORT || 8080;
 
 let app = express();
-let db = require('./models');
+let db = require("./models");
 
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.engine("handlebars", exphbrs({
+app.engine(
+  "handlebars",
+  exphbrs({
     defaultLayout: "main"
-}));
+  })
+);
 app.set("view engine", "handlebars");
 
-let routes = require('./controllers/homepage_controller');
+let routes = require("./controllers/homepage_controller");
 
 app.use(routes);
 
-db.sequelize.sync({
+db.sequelize
+  .sync({
     force: false
-}).then(() => {
+  })
+  .then(() => {
     app.listen(PORT, () => {
-        console.log('App listening on Port: ' + PORT);
-    })
-})
+      console.log("App listening on Port: " + PORT);
+    });
+  });
