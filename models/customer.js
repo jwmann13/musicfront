64fd3671736398023ee08e66f2aef51be7331870
Jeffrey Mann <jwmann22@gmail.com>
2019-12-05 21:13:42 -0500

    added formatting to product block

--- models/customer.js
+++ models/customer.js
@@ -1,26 +1,27 @@
 module.exports = function(sequelize, DataTypes) {
   var customer = sequelize.define("Customer", {
     userName: {
-        type: DataTypes.STRING(45)
+      type: DataTypes.STRING(45)
     },
-    userPassword: { 
-        type: DataTypes.STRING(45) },
+    userPassword: {
+      type: DataTypes.STRING(45)
+    },
 
     firstName: {
-        type: DataTypes.STRING(45)
+      type: DataTypes.STRING(45)
     },
     lastName: {
-        type: DataTypes.STRING(45)
+      type: DataTypes.STRING(45)
     },
     addres: {
-        type: DataTypes.STRING(100)
+      type: DataTypes.STRING(100)
     },
     email: {
-        type: DataTypes.STRING(45),
+      type: DataTypes.STRING(45)
     },
     cardNumber: {
-        type: DataTypes.INTEGER
+      type: DataTypes.INTEGER
     }
-    });
+  });
   return customer;
 };
