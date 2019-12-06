6dcb0e551be7de1c7eed1b7379c9391f69365933
Nico Loparco <nicoloparco75@gmail.com>
2019-12-05 19:18:57 -0500

    cleaned up models

--- models/customer.js
+++ models/customer.js
@@ -1,13 +1,29 @@
-module.exports = function (sequelize, DataTypes) {  
-    var customer = sequelize.define("Customer", {
-        id: DataTypes.INTEGER,
-        userName: DataTypes.STRING(45),
-        userPassword: DataTypes.STRING(45),
-        firstName: DataTypes.STRING(45),
-        lastName: DataTypes.STRING(45),
-        addres: DataTypes.STRING(100),
-        email: DataTypes.STRING(45),
-        cardNumber: DataTypes.INTEGER
+module.exports = function(sequelize, DataTypes) {
+  var customer = sequelize.define("Customer", {
+    id: {
+      type: DataTypes.INTEGER
+    },
+    userName: {
+        type: DataTypes.STRING(45)
+    },
+    userPassword: { 
+        type: DataTypes.STRING(45) },
+
+    firstName: {
+        type: DataTypes.STRING(45)
+    },
+    lastName: {
+        type: DataTypes.STRING(45)
+    },
+    addres: {
+        type: DataTypes.STRING(100)
+    },
+    email: {
+        type: DataTypes.STRING(45),
+    },
+    cardNumber: {
+        type: DataTypes.INTEGER
+    }
     });
-    return customer;
+  return customer;
 };
