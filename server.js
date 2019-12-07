b49b0d121ea4c31c0ef606dfc24ce97961f16649
Jeffrey Mann <jwmann22@gmail.com>
2019-12-07 10:46:39 -0500

    some styling

--- server.js
+++ server.js
@@ -19,7 +19,18 @@ app.use(express.static(path.join(__dirname, "public")));
 app.engine(
   "handlebars",
   exphbrs({
-    defaultLayout: "main"
+    defaultLayout: "main",
+    helpers: {
+      ifeq: function(a, b, options){
+        if (a == b) {
+          return options.fn(this);
+          }
+        return options.inverse(this);
+      },
+      toLowerCase: function(str) {
+        return str.toLowerCase();
+      }
+    }
   })
 );
 app.set("view engine", "handlebars");
