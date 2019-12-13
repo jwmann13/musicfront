91557eb7ac1308e41b4e06499c9c87c1fef2cbc1
Nico Loparco <nicoloparco75@gmail.com>
2019-12-12 21:33:30 -0500

    shopping cart functionality

--- public/js/productInfo.js
+++ public/js/productInfo.js
@@ -1,7 +1,7 @@
 $("#add-button").on("click", function() {
-    let id = $(this).data("id");
-    
-    $.get(`/products/info/${id}`, (product) => {
-        console.log(product)
-    });
-})
+  let id = $(this).data("id");
+
+  $.get(`/products/info/${id}`, product => {
+    console.log(product);
+  });
+});
