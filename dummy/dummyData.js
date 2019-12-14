ed8e6528db1d6bdcd8492953b7182ad8f9d89779
Nico Loparco <nicoloparco75@gmail.com>
2019-12-13 20:39:03 -0500

    updated styling and images

--- dummy/dummyData.js
+++ dummy/dummyData.js
@@ -1,96 +1,96 @@
 module.exports = {
   products: [
     {
-      brand: "gibson",
-      name: "bad guitar",
-      model: "les paul",
-      instrumentFamily: "electric guitar",
+      brand: "Gibson",
+      name: "Bad Guitar",
+      model: "Les Paul",
+      instrumentFamily: "Electric Guitar",
       description: "a really bad stinky guitar",
       price: 10.0,
       rating: 1.2,
-      photo: "http://lorempixel.com/g/200/200"
+      photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Full_front_R9_Les_Paul.jpg/440px-Full_front_R9_Les_Paul.jpg"
     },
     {
-      brand: "fender",
-      name: "good guitar",
-      model: "stratocaster",
-      instrumentFamily: "electric guitar",
+      brand: "Fender",
+      name: "Good Guitar",
+      model: "Stratocaster",
+      instrumentFamily: "Electric guitar",
       description: "a really good guitar you gotta love it",
       price: 103.5,
       rating: 4.6,
-      photo: "http://lorempixel.com/g/200/200"
+      photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/1958_Fender_Stratocaster.jpg/440px-1958_Fender_Stratocaster.jpg"
     },
     {
-      brand: "yamaha",
-      name: "our favorite 80s synth",
-      model: "dx-7",
-      instrumentFamily: "synthesizer",
+      brand: "Yamaha",
+      name: "Our Favorite 80s Synth",
+      model: "DX-7",
+      instrumentFamily: "Synthesizer",
       description: "bleep bloop i've had the time of my life bleep bloop",
       price: 2000.0,
       rating: 3.9,
-      photo: "http://lorempixel.com/g/200/200"
+      photo: "https://www.electronicbeats.net/app/uploads/2018/02/Yamaha-DX7-Keyboard-11.jpg"
     },
     {
-      brand: "strativarius",
-      name: "very fancy violin",
-      model: "red violin",
-      instrumentFamily: "string instrument",
-      description: "this things been around forever",
+      brand: "Strativarius",
+      name: "Very Fancy Violin",
+      model: "Red Violin",
+      instrumentFamily: "String Instrument",
+      description: "this thing has been around forever",
       price: 1020.9,
       rating: 4.2,
-      photo: "http://lorempixel.com/g/200/200"
+      photo: "http://www.elizabethpitcairn.com/wp-content/uploads/2014/12/archived-pic-2.jpg"
     },
     {
-      brand: "rickenbocker",
-      name: "that weird curvy bass",
-      model: "4000 bass",
-      instrumentFamily: "bass guitar",
+      brand: "Rickenbocker",
+      name: "That Weird Curvy Bass",
+      model: "4000 Bass",
+      instrumentFamily: "Bass Guitar",
       description: "i think paul mccartney played this guy",
       price: 500.0,
       rating: 2.9,
-      photo: "http://lorempixel.com/g/200/200"
+      photo: "https://images.reverb.com/image/upload/s--ZOLu8jxh--/f_auto,t_supersize/v1567028321/xmh6y49eiqzoxs5uoivs.jpg"
     },
     {
-      brand: "yamaha",
-      name: "beautiful saxophone",
+      brand: "Yamaha",
+      name: "Beautiful Saxophone",
       model: "YAS-827",
-      instrumentFamily: "woodwind",
+      instrumentFamily: "Woodwind",
       description: "toot toot saxamaphone",
       price: 368.3,
       rating: 3.8,
-      photo: "http://lorempixel.com/g/200/200"
+      photo: "https://images-na.ssl-images-amazon.com/images/I/81chkRcnGvL._SY879_.jpg"
     },
     {
       brand: "Korg",
-      name: "compact polyphonic synth",
-      model: "minilogue",
-      instrumentFamily: "synthesizer",
+      name: "Compact Polyphonic Synth",
+      model: "Minilogue",
+      instrumentFamily: "Synthesizer",
       description: "recent powerhouse poly synth from korg",
       price: 450.0,
       rating: 4.0,
-      photo: "http://lorempixel.com/g/200/200"
+      photo: "https://www.thomann.de/pics/bdb/428147/12886867_800.jpg"
     },
     {
       brand: "Getzen",
-      name: "decent trumpet",
+      name: "Decent Trumpet",
       model: "590S-S",
-      instrumentFamily: "brass",
+      instrumentFamily: "Brass",
       description:
         "play the impression that i get by the mighty mighty bosstones",
       price: 150.0,
       rating: 1.8,
-      photo: "http://lorempixel.com/g/200/200"
+      photo: "https://i5.walmartimages.com/asr/ea5f2caa-cd0c-46b7-a06e-82beb41a95a5_1.6bf16bea53b2822b1cab91857a5791c0.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF"
     },
     {
-      brand: "martin",
-      name: "vintage acoustic",
+      brand: "Martin",
+      name: "Vintage Acoustic",
       model: "D-18 Authentic 1939",
-      instrumentFamily: "acoustic guitar",
+      instrumentFamily: "Acoustic Guitar",
       description:
         "thankfully not the one kurt russel smashed in hateful eight",
       price: 986.3,
       rating: 4.9,
-      photo: "http://lorempixel.com/g/200/200"
+      photo: "https://images.reverb.com/image/upload/s--A1Rfp_YJ--/a_exif,c_limit,e_unsharp_mask:80,f_auto,fl_progressive,g_south,h_620,q_90,w_620/v1418677072/cywa1qyvfrgcan62n9sk.jpg"
     }
   ],
   customers: [],
